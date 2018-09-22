aee8847adceb2e5f91c03b98345a1664ff72c460
Jasper Roel <jasperroel+c9@gmail.com>
2018-09-22 16:59:25 +0000

    Fixes from 2017 (unverified)

--- 2013/kml.php
+++ 2013/kml.php
@@ -1,6 +1,8 @@
 <?php
 if(!defined("opoiLoaded")) die('Incorrect or unknown use of application');
 
+// http://www.zonums.com/online/kmlArea/
+
 function getStyle($accuracy = 0.0) {
     $a = floatval($accuracy);
     if ($a === 0.0) {
@@ -35,52 +37,71 @@ function getColor($accuracy = 0.0) {
 
 function getStyles() {
     $styles = '';
-    $styles .= '<Style id="greenIcon"><IconStyle><scale>1.0</scale><Icon><href>https://www.google.com/mapfiles/ms/micons/green.png</href></Icon></IconStyle></Style>';
-    $styles .= '<Style id="blueIcon"><IconStyle><scale>1.0</scale><Icon><href>https://www.google.com/mapfiles/ms/micons/blue.png</href></Icon></IconStyle></Style>';
-    $styles .= '<Style id="yellowIcon"><IconStyle><scale>1.0</scale><Icon><href>https://www.google.com/mapfiles/ms/micons/yellow.png</href></Icon></IconStyle></Style>';
-    $styles .= '<Style id="redIcon"><IconStyle><scale>1.0</scale><Icon><href>https://www.google.com/mapfiles/ms/micons/red.png</href></Icon></IconStyle></Style>';
+    $styles .= '<Style id="line">
+      <LineStyle>
+        <color>7f0000B2</color>
+        <width>3</width>
+      </LineStyle>
+      <PolyStyle>
+        <color>7f00ff00</color>
+      </PolyStyle>
+    </Style>';
     return $styles;
 }
 
-if (isset($_GET ['riderId'])) {
-    $riderId = $_GET ['riderId'];
+$sessionId = $authMgr->getSessionId();
+if (isset($_GET['sessionId'])) {
     $sessionId = $_GET['sessionId'];
-    $authMgr->setSessionId($sessionId);
-    $riderTeam = $driver->getRider($riderId);
+}
+$authMgr->setSessionId($sessionId);
+
+
+header('Content-Type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<kml xmlns="http://www.opengis.net/kml/2.2">';
+echo '<Document>';
+echo getStyles();
+echo '<name>Jotihunt ' + date('Y') +'</name>';
+echo '<description></description>';
+
+$max = 20;
+$offset = (isset($_GET['offset']) ? $_GET['offset'] : 0 );
+
+// Get all riders
+$riders = array();
+if (isset($_GET['hunterId'])) {
+    $riders[] = $driver->getRider($_GET['hunterId']);
+} else {
+    $riders = $driver->getAllRiders();
+}
+foreach ($riders as $riderTeam) {
     if (null == $riderTeam) {
-        return;
+        continue;
     }
+
     if (isset($_GET ['from']) && isset($_GET ['to'])) {
         $locations = $driver->getRiderLocationWithDateRange($riderTeam->getId(), $_GET ['from'], $_GET ['to']);
     } else {
         $locations = $driver->getRiderLocation($riderTeam->getId());
     }
+
+    if (count($locations) == 0) {
+        continue;
+    }
     
-    //
-    header('Content-Type: text/xml');
-    echo '<?xml version="1.0" encoding="UTF-8"?>';
-    echo '<kml xmlns="http://www.opengis.net/kml/2.2">';
-    echo '<Document>';
-    echo getStyles();
-    echo '<name>' . $riderTeam->getUser()->getDisplayName() . '</name>';
-    echo '<description></description>';
-    foreach ( $locations as $location ) {
-        echo '<Placemark id="'.$riderTeam->getUser()->getId().'-'. $location->getId() . '">';
-        echo '<name>Location ID: '.$location->getId().'</name>';
-        // echo '<styleUrl>' . getStyle($location->getAccuracy()) . '</styleUrl>';
-        // echo '<Style><BalloonStyle><bgColor>' . getColor($location->getAccuracy()) . '</bgColor></BalloonStyle></Style>';
-        echo '<Point><coordinates>';
-        echo $location->getLongitude() . ',' . $location->getLatitude();
-        echo '</coordinates></Point>';
-        echo '<description><![CDATA[';
-        echo '<div>Datum: ' . $location->getTime() . '<br />Provider: ' . $location->getProvider() . ' (' . $location->getAccuracy() . ')</div>';
-        echo ']]></description>';
-        echo '</Placemark>';
+    if ($offset > 0) {
+        $offset--;
+        continue;
+    }
+    $max--;
+    if ($max == 0) {
+        break;
     }
     
     // Line
-    echo '<Placemark id="coordinates">';
-    echo '<name>Coordinates</name>';
+    echo '<Placemark id="coordinates_'.$riderTeam->getUser()->getId().'">';
+    echo '<name>'.$riderTeam->getUser()->getDisplayName().'</name>';
+    echo '<styleUrl>#line</styleUrl>';
     echo '<LineString>';
     echo '<extrude>1</extrude>';
     echo '<tessellate>1</tessellate>';
@@ -92,6 +113,6 @@ if (isset($_GET ['riderId'])) {
     echo '</coordinates>';
     echo '</LineString>';
     echo '</Placemark>';
-    
-    echo '</Document></kml>';
 }
+
+echo '</Document></kml>';
