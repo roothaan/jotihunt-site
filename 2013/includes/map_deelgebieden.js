e14dc3674716e5e717ca4a82805e5ace9ddc1ece
Jasper Roel <jasperroel@gmail.com>
2022-10-14 12:15:22 +0200

    fix(2022): allow for flushing KML

--- 2013/includes/map_deelgebieden.js
+++ 2013/includes/map_deelgebieden.js
@@ -1,11 +1,15 @@
 function drawDeelgebieden(map, eventId) {
-    var kmlUrl =  window.location.origin + '/deelgebieden-kml/' + eventId
+    const cacheToken = sessionStorage.getItem('jotihunt.kmlCache')
+    if (!cacheToken) {
+        sessionStorage.setItem('jotihunt.kmlCache', Math.random().toString(36).substr(2))
+    }
+    const kmlUrl = window.location.origin + '/deelgebieden-kml/' + eventId + '?cb=' + sessionStorage.getItem('jotihunt.kmlCache');
     console.log("Using kmlUrl: " + kmlUrl);
-    var kmlOptions = {
-      suppressInfoWindows: false,
-      preserveViewport: false,
-      map: map
+    const kmlOptions = {
+        suppressInfoWindows: false,
+        preserveViewport: false,
+        map: map
     };
-    
+
     return new google.maps.KmlLayer(kmlUrl, kmlOptions);
 }
