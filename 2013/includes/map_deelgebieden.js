9279349d4bc27087e9e55d9e748c7c7d9b06817e
Jasper Roel <jasperroel@users.noreply.github.com>
2022-10-14 23:17:27 +0200

    Update map_deelgebieden.js

--- 2013/includes/map_deelgebieden.js
+++ 2013/includes/map_deelgebieden.js
@@ -1,8 +1,8 @@
 function drawDeelgebieden(map, eventId) {
-    let params = ''
+    let params = '?event=2022-01'
     const cacheToken = sessionStorage.getItem('jotihunt.kmlCache')
     if (cacheToken) {
-        params = '?cb=' + cacheToken
+        params += '&cb=' + cacheToken
     }
 
     const kmlUrl = window.location.origin + '/deelgebieden-kml/' + eventId + params
